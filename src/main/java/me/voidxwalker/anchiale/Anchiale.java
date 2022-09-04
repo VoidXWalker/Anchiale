1508a658925ec058c4019326bd01f2a15c6dc6f7
KingContaria <98704937+KingContaria@users.noreply.github.com>
2022-09-04 03:11:56 +0200

    1.8.0 - 1.12.2 & nicer button
    adds compatibility for versions 1.8.0 to 1.12.2
    i know the effects get less and less noticable as the versions progress but it was compatible anyway and still has a (very small however in 1.12) impact so i just included it cuz why not
    also makes the button render smaller (adjusting to the translation string size) and not touch the edge of the window, making it more like the fastreset button

--- src/main/java/me/voidxwalker/anchiale/Anchiale.java
+++ src/main/java/me/voidxwalker/anchiale/Anchiale.java
@@ -1,14 +1,9 @@
 package me.voidxwalker.anchiale;
 
-import net.fabricmc.api.ClientModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Anchiale implements ClientModInitializer {
+public class Anchiale {
     public static final Logger LOGGER = LogManager.getLogger();
     public static boolean fastReset = false;
-    @Override
-    public void onInitializeClient() {
-
-    }
 }
